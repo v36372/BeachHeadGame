833f553ecefdc38b243ad672ad8df8404cc53d1f
Nguyen Trong Tin <nguyentrongtin2331@gmail.com>
2015-06-11 00:14:13 +0700

    * modify some default settings

--- Source/BeachHead/BulletProjectile.cpp
+++ Source/BeachHead/BulletProjectile.cpp
@@ -12,7 +12,7 @@ ABulletProjectile::ABulletProjectile()
 
 	// Use a sphere as a simple collision representation
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
-	CollisionComp->InitSphereRadius(5.0f);
+	CollisionComp->InitSphereRadius(15.0f);
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
 	CollisionComp->OnComponentHit.AddDynamic(this, &ABulletProjectile::OnHit);		// set up a notification for when this component hits something blocking
 
@@ -22,8 +22,8 @@ ABulletProjectile::ABulletProjectile()
 	// Use a ProjectileMovementComponent to govern this projectile's movement
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
 	ProjectileMovement->UpdatedComponent = CollisionComp;
-	ProjectileMovement->InitialSpeed = 3000.f;
-	ProjectileMovement->MaxSpeed = 3000.f;
+	ProjectileMovement->InitialSpeed = 4000.f;
+	ProjectileMovement->MaxSpeed = 4000.f;
 	ProjectileMovement->bRotationFollowsVelocity = true;
 	ProjectileMovement->bShouldBounce = true;
 
@@ -50,12 +50,12 @@ void ABulletProjectile::Tick( float DeltaTime )
 
 void ABulletProjectile::OnHit(AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red, TEXT("ABeachHeadProjectile::OnHit"));
+	UE_LOG(LogTemp, Warning, TEXT("ABulletProjectile::OnHit"));
 	// Only add impulse and destroy projectile if we hit a physics
 	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
-		GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red, TEXT("ABeachHeadProjectile::OnHit1231212423234234"));
+		UE_LOG(LogTemp, Warning, TEXT("ABulletProjectile::OnHit 123123123123"));
 		Destroy();
 	}
 }
